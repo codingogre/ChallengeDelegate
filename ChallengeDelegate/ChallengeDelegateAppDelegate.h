86205596b4e2353f5f33167f795671ea1e81e84b
Shawn Hooton <shooton@gopivotal.com>
2014-02-09 14:16:47 -0500

    Initial Commit

---
+++ ChallengeDelegate/ChallengeDelegateAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ChallengeDelegateAppDelegate.h
+//  ChallengeDelegate
+//
+//  Created by Shawn Hooton on 2/9/14.
+//  Copyright (c) 2014 Sixpanel LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ChallengeDelegateAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ ChallengeDelegate/ChallengeDelegateAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  ChallengeDelegateAppDelegate.m
+//  ChallengeDelegate
+//
+//  Created by Shawn Hooton on 2/9/14.
+//  Copyright (c) 2014 Sixpanel LLC. All rights reserved.
+//
+
+#import "ChallengeDelegateAppDelegate.h"
+
+@implementation ChallengeDelegateAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
